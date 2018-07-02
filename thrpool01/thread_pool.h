909ad671e476646e8845e0022a29c8adce1b282e
uderos <uderos@ilww.com>
2018-07-02 14:21:36 -0400

    cleanup

--- thrpool01/thread_pool.h
+++ thrpool01/thread_pool.h
@@ -19,13 +19,11 @@ class thread_pool
 		void execute(data_ptr_t data_ptr);
 
 		void terminate_and_wait();
-		void terminate_no_wait();
+//		void terminate_no_wait();
 
-		// void dbg_dump(std::ostream & os) const;
 
 	private:
 
-		using thread_ptr_t = std::unique_ptr<std::thread>;
 		using lock_t = std::unique_lock<std::mutex>;
 
 		mutable std::condition_variable m_condition_variable;
@@ -35,7 +33,7 @@ class thread_pool
 
 		std::atomic_bool m_termination_flag;
 
-		std::vector<thread_ptr_t> m_working_threads;
+		std::vector<std::thread> m_working_threads;
 		std::queue<data_ptr_t> m_working_data_queue;
 
 		void m_thread_fn(const int tid);
@@ -54,9 +52,8 @@ thread_pool<WORK_FN_T, WORK_DATA_T>::thread_pool(WORK_FN_T & work_fn,
 	m_termination_flag(false)
 {
 	for (std::size_t i = 0; i < num_threads; ++i)
-		m_working_threads.push_back(
-			std::make_unique<std::thread>(
-				&thread_pool<WORK_FN_T, WORK_DATA_T>::m_thread_fn, this, i));
+		m_working_threads.emplace_back(
+			&thread_pool<WORK_FN_T, WORK_DATA_T>::m_thread_fn, this, i);
 }
 
 template <typename WORK_FN_T, typename WORK_DATA_T>
@@ -80,28 +77,25 @@ void thread_pool<WORK_FN_T, WORK_DATA_T>::terminate_and_wait()
 
 	m_condition_variable.notify_all();
 
-//	std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // UBEDEBUG
-
-
-	for (auto & thr_ptr : m_working_threads)
+	for (auto & t : m_working_threads)
 	{
-		if ((*thr_ptr).joinable())
-			(*thr_ptr).join();
+		if (t.joinable())
+			t.join();
 	}
 
 	m_working_threads.clear();
 }
 
-template <typename WORK_FN_T, typename WORK_DATA_T>
-void thread_pool<WORK_FN_T, WORK_DATA_T>::terminate_no_wait()
-{
-	for (auto & thr_ptr : m_working_threads)
-		(*thr_ptr).detach();
-
-	m_termination_flag = true;
-
-	m_condition_variable.notify_all();
-}
+//template <typename WORK_FN_T, typename WORK_DATA_T>
+//void thread_pool<WORK_FN_T, WORK_DATA_T>::terminate_no_wait()
+//{
+//	for (auto & t : m_working_threads)
+//		t.detach();
+//
+//	m_termination_flag = true;
+//
+//	m_condition_variable.notify_all();
+//}
 
 template <typename WORK_FN_T, typename WORK_DATA_T>
 void thread_pool<WORK_FN_T, WORK_DATA_T>::m_thread_fn(const int tid)
